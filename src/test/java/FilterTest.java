ca2a7a9d030afad6a84db6d44d2b3910b0c47dc1
YS-Yue <sun.yue1@northeastern.edu>
2022-03-14 00:27:29 -0700

    Filter class without optional attribute

--- src/test/java/FilterTest.java
+++ src/test/java/FilterTest.java
@@ -5,72 +5,87 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class FilterTest {
-    List<Item> allHotels = new ArrayList<>();;
-    List<Item> allActivityItem = new ArrayList<>();
-    List<Item> allAirport = new ArrayList<>();
-    List<Activity> allActivities = new ArrayList<>();
+
+    List<Item> allActivityItemList = new ArrayList<>();
+    List<Item> allAirportItemList = new ArrayList<>();
+    List<Item> allHotelsItemList = new ArrayList<>();
+
     Location location1;
     Location location2;
     Location location3;
     Location location0;
-
     Hotel hotel1;
-    Item activity1;
-    Item activity2;
-    Item activity3;
+    Activity activity1;
+    Activity activity2;
+    Activity activity3;
 
     @BeforeEach
     void setUp() {
+        // Define some Location objects
         location0 = new Location(-122.1830, 47.6670);
         location1 = new Location(-122.1500, 47.6800);
         location2 = new Location(-123.1830, 48.6670);
         location3 = new Location(-122.1831, 60.6671);
 
+
+        // Populating List<Item> of hotels
         hotel1 = new Hotel(location0, 4.5F);
+        allHotelsItemList.add(hotel1);
+        allHotelsItemList.add(new Hotel(location1, 5.0F));
+        allHotelsItemList.add(new Hotel(location2, 3.0F));
+        allHotelsItemList.add(new Hotel(location3, 3.5F));
+
+        // Populating List<Item> of activities
         activity1 = new Activity(location1, 4.9F, 20D, 75D);
         activity2 = new Activity(location2, 4.7F, 30D, 75D);
         activity3 = new Activity(location3, 3.0F, 40D, 75D);
+        allActivityItemList.add(activity1);
+        allActivityItemList.add(activity2);
+        allActivityItemList.add(activity3);
 
-        allActivities.add((Activity) activity1);
-        allActivities.add((Activity)activity2);
-        allActivities.add((Activity)activity3);
-
-        allActivityItem.add(activity1);
-        allActivityItem.add(activity2);
-        allActivityItem.add(activity3);
+        // Populating List<Item> of airports
+        allAirportItemList.add(new Airport(location1, 3.1F));
+        allAirportItemList.add(new Airport(location2, 3.5F));
+        allAirportItemList.add(new Airport(location3, 3.9F));
 
     }
 
     @Test
     void ratingFilter() {
-        allHotels.add(hotel1);
-        allHotels.add(new Hotel(location1, 5.0F));
-        allHotels.add(new Hotel(location2, 3.0F));
-        allHotels.add(new Hotel(location3, 3.5F));
 
-        Assertions.assertEquals(2, Filter.ratingFilter(allHotels, 3.2F, 4.9F).size());
+        // Hotels Rating Filter
+        Assertions.assertEquals(2, Filter.ratingFilter(allHotelsItemList, 3.2F, 4.9F).size());
+
+        // Activities Rating Filter
+        Assertions.assertEquals(2, Filter.ratingFilter(allActivityItemList, 2.2F, 4.8F).size());
+
+        // Airports Rating Filter
+        Assertions.assertEquals(0, Filter.ratingFilter(allAirportItemList, 4F, 5F).size());
 
     }
 
     @Test
     void locationFilter() {
         System.out.println("Distance between hotel1 and activity1: " + Filter.calculateDistance(hotel1,activity1));
-        System.out.println("Distance between hotel1 and activity2: " + Filter.calculateDistance(hotel1,activity2));
-        System.out.println("Distance between hotel1 and activity3: " + Filter.calculateDistance(hotel1,activity3));
-        System.out.println(Filter.locationFilter(allActivityItem,hotel1,300));
+        // Distance between hotel1 and activity1: 2.8626150035471425
 
-        Assertions.assertEquals(2,Filter.locationFilter(allActivityItem,hotel1,300).size());
+        System.out.println("Distance between hotel1 and activity2: " + Filter.calculateDistance(hotel1,activity2));
+        // Distance between hotel1 and activity2: 133.65510383822868
+
+        System.out.println("Distance between hotel1 and activity3: " + Filter.calculateDistance(hotel1,activity3));
+        // Distance between hotel1 and activity3: 1445.5451658861589
+
+        Assertions.assertEquals(2,Filter.locationFilter(allActivityItemList,hotel1,300).size());
     }
 
     @Test
     void priceFilter() {
         List<Activity> activityList = new ArrayList<>();
-        for (Item item : allActivityItem) {
+        for (Item item : allActivityItemList) {
             activityList.add((Activity) item);
         }
+        // The input and output of Filter.priceFilter method are both List<Activity> instead of List<Item>
         Assertions.assertEquals(1, Filter.priceFilter(activityList,25,35).size());
     }
 }
